c0a046e9ad05f240759bd92acd5eb0607791af9c
Anes Djebili <34942214+anesdjebili@users.noreply.github.com>
2024-09-15 18:56:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 // Smooth scroll to section
 $(document).on('click', 'a[href^="#"]', function(event) {
-	event.preventDefault();
-	$('html, body').animate({
-		scrollTop: $($.attr(this, 'href')).offset().top
-	}, 500);
+  event.preventDefault();
+  var target = $($.attr(this, 'href'));
+  $('html, body').animate({
+    scrollTop: target.offset().top - 50 // add a small offset to account for padding or margins
+  }, 500, 'swing'); // add a easing effect to make the animation smoother
 });
 
 // Gallery lightbox
 $('.gallery-item').click(function() {
-	var image = $(this).find('img').attr('src');
-	$('body').append('<div class="lightbox"><img src="' + image + '"></div>');
-});
-
-$(document).on('click', '.lightbox', function() {
-	$(this).remove();
+  var image = $(this).find('img').attr('src');
+  var lightbox = $('<div class="lightbox"><img src="' + image + '"></div>');
+  lightbox.appendTo('body'); // use appendTo instead of append for better performance
+  lightbox.on('click', function() {
+    $(this).remove();
+  });
 });
